98559b6124645652e3bcce411b4b99a14b79c3d3
MD. Shimul <mdshemul480@gmail.com>
2023-07-12 11:53:21 +0600

    adding users route

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,9 @@
 const { sampleHandler } = require("../handlers/routeHandler/sampleHandler");
+const { userHandler } = require("../handlers/routeHandler/userHandler");
 
 const routes = {
   sample: sampleHandler,
+  users: userHandler,
 };
 
 module.exports = routes;
