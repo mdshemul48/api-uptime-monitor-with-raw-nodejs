4db5d9af494c2b92e9d2166728979e2cf31a56ee
MD. Shimul <mdshemul480@gmail.com>
2023-07-12 12:17:33 +0600

    adding env

--- helpers/environments.js
+++ helpers/environments.js
@@ -3,12 +3,15 @@ const environments = {};
 environments.production = {
   port: 5000,
   envName: "production",
+  secretKey: "this is going to be really good.",
 };
 
 environments.staging = {
   port: 3000,
   envName: "staging",
+  secretKey: "another good one",
 };
+
 const currentEnvironment =
   typeof process.env.NODE_ENV === "string"
     ? process.env.NODE_ENV.trim()
